ad5024b25d5e4b008b3a9d50cb49f837476bd1be
root <root@DESKTOP-N0FEVS1.localdomain>
2020-05-19 13:25:52 +0800

    pyflink add json jar

--- install_pyflink.sh
+++ install_pyflink.sh
@@ -4,10 +4,13 @@ source ./common.sh
 
 download_url_bz2=http://45.76.247.122:9000/public/_bz2.cpython-VERSION-x86_64-linux-gnu.so
 download_url_flink=http://45.76.247.122:9000/public/apache-flink-1.10.1.tar.gz
-lib_url_csv=https://repo1.maven.org/maven2/org/apache/flink/flink-csv/1.10.0/flink-csv-1.10.0-sql-jar.jar
-lib_url_jdbc=https://repo1.maven.org/maven2/org/apache/flink/flink-jdbc_2.11/1.10.0/flink-jdbc_2.11-1.10.0.jar
-lib_url_kfk=https://repo1.maven.org/maven2/org/apache/flink/flink-sql-connector-kafka_2.11/1.10.0/flink-sql-connector-kafka_2.11-1.10.0.jar
-lib_url_mysql=https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.19/mysql-connector-java-8.0.19.jar
+lib_url_csv=https://repo.maven.apache.org/maven2/org/apache/flink/flink-csv/1.10.0/flink-csv-1.10.0.jar
+lib_url_csv_sql=https://repo.maven.apache.org/maven2/org/apache/flink/flink-csv/1.10.0/flink-csv-1.10.0-sql-jar.jar
+lib_url_json=https://repo.maven.apache.org/maven2/org/apache/flink/flink-json/1.10.0/flink-json-1.10.0.jar
+lib_url_json_sql=https://repo.maven.apache.org/maven2/org/apache/flink/flink-json/1.10.0/flink-json-1.10.0-sql-jar.jar
+lib_url_jdbc=https://repo.maven.apache.org/maven2/org/apache/flink/flink-jdbc_2.11/1.10.0/flink-jdbc_2.11-1.10.0.jar
+lib_url_kfk=https://repo.maven.apache.org/maven2/org/apache/flink/flink-sql-connector-kafka_2.11/1.10.0/flink-sql-connector-kafka_2.11-1.10.0.jar
+lib_url_mysql=https://repo.maven.apache.org/maven2/mysql/mysql-connector-java/8.0.19/mysql-connector-java-8.0.19.jar
 lib_path=
 
 function prepare_bz2()
@@ -79,6 +82,9 @@ function pip_install_flink()
 function complement_lib()
 {
     wget ${lib_url_csv} -O ${lib_path}"/pyflink/lib/"${lib_url_csv##*/}
+    wget ${lib_url_csv_sql} -O ${lib_path}"/pyflink/lib/"${lib_url_csv_sql##*/}
+    wget ${lib_url_json} -O ${lib_path}"/pyflink/lib/"${lib_url_json##*/}
+    wget ${lib_url_json_sql} -O ${lib_path}"/pyflink/lib/"${lib_url_json_sql##*/}
     wget ${lib_url_jdbc} -O ${lib_path}"/pyflink/lib/"${lib_url_jdbc##*/}
     wget ${lib_url_kfk} -O ${lib_path}"/pyflink/lib/"${lib_url_kfk##*/}
     wget ${lib_url_mysql} -O ${lib_path}"/pyflink/lib/"${lib_url_mysql##*/}
